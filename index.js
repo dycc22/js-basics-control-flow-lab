af0e1407165dfd6b29b9c284972df838ede7b51f
David <dycchang@gmail.com>
2021-04-08 11:26:23 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+// Write your code here!
+
+if (length < 400)
+  return 'This one is on me!';
+else if (length > 2000 && length < 2500)
+  return 'I will gladly take your thirty bucks.';
+else if (length > 2500) 
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  return tip === 'generous' ? 'Thank you so much.' 
+         : tip === 'not as generous' ? 'Thank you.'
+         : 'Bye.';
 }
